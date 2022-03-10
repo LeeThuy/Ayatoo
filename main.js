5478e0d0d7c77482a406b58500ce7e7748fa92f1
Lê Thủy <63569330+LeeThuy@users.noreply.github.com>
2022-03-10 14:03:49 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+function trangchu()
+{
+    window.open("https://genshin.hoyoverse.com/vi/home","_blank");
+}
+function tintuc()
+{
+    window.open("https://genshin.hoyoverse.com/vi/news","_blank");
+}
+function nhanvat()
+{
+    window.open("https://genshin.hoyoverse.com/vi/character/mondstadt?char=0","_blank");
+}
+function khac(){
+    window.open("https://www.hoyolab.com/home?utm_source=officialweb&utm_medium=game&utm_campaign=nav&utm_id=2","_blank");
+}
+function Open() {
+    window.open("https://genshin.hoyoverse.com/vi/home","_blank");
+}
+function danhsach1(){
+    document.getElementById('danhsach').style.display="inline";
+}
+function danhsach2(){
+    document.getElementById('danhsach').style.display="none";
+}
+function audio(){
+    document.getElementById('on').play().display = "none";
+}
+// -----------------------------------------------------------------
